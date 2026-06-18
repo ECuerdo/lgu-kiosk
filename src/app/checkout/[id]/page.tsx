969c2325a5a1259b7852f978f5def8358a7456db
jhonemilmnilo <nilojhonemil1@gmail.com>
2026-06-18 17:12:35 +0800

    fix: redirect cancel url to cedula list page if transaction is cedula

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -197,9 +197,14 @@ export default function CheckoutPage() {
         });
       } else if (paymentStatus === "cancelled") {
         showToast("Payment checkout cancelled.", "warning");
+        if (transaction && transaction.type?.code?.startsWith("CEDULA")) {
+          setTimeout(() => {
+            router.push("/modules/cedula");
+          }, 1500);
+        }
       }
     }
-  }, [searchParams, userId, id, transaction, handleBackRedirect]);
+  }, [searchParams, userId, id, transaction, handleBackRedirect, router]);
 
 
 
@@ -261,6 +266,7 @@ export default function CheckoutPage() {
           transactionId: transaction.id,
           reference: `${transaction.type?.name || "Municipal Fee"} CheckOut`,
           redirectPath: `/checkout/${transaction.id}`,
+          cancelPath: transaction.type?.code?.startsWith("CEDULA") ? "/modules/cedula" : `/checkout/${transaction.id}`,
         }),
       });
 
