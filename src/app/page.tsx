2e202b22aba22db57197c84cbb0be1bfd86f6f26
Rogelio Angelo V. Duque <128900461+RogelioAngelo@users.noreply.github.com>
2026-07-09 10:19:57 +0800

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,1 @@
-import KioskSlideshow from "@/components/KioskSlideshow";
-import RfidOverlay from "@/components/RfidOverlay";
-import KioskActivationGate from "@/components/shared/KioskActivationGate";
-
-export default function Home() {
-  return (
-    <KioskActivationGate>
-      <KioskSlideshow />
-      <RfidOverlay />
-    </KioskActivationGate>
-  );
-}
+﻿// placeholder
